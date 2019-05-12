6b411daad907b183b10a6a58cd817be04f6df5f3
David Cuadrado <dacufe1718daw2vib@gmail.com>
2019-05-12 14:11:52 +0200

    conflicte 1 entre br0 i master

--- mitjana00.c
+++ mitjana00.c
@@ -22,6 +22,6 @@ int main() {
 	}
 	mitjana=suma/numero_repeticions;
 	printf("La mitjana resultant és: %.2f\n",mitjana);	
-	printf("OPeracio finalitzada amb exit\n");
+	printf("Operació finalitzada amb èxit\n");
 	return 0;
 }
